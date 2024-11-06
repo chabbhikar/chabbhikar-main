eb923a94d5523a898533a93823953077d18dbddf
Akshay Kumar <akshay.official.dev@gmail.com>
2024-11-07 00:08:26 +0530

    added small changes and improve folder structure

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,17 +2,22 @@
 // import { Button } from "@repo/ui/button";
 // import styles from "./page.module.css";
 
-import Carousel from "./components/carousel";
-const items = [
-  <img src="image1.jpg" alt="Slide 1" />,
-  <img src="image2.jpg" alt="Slide 2" />,
-  <img src="image3.jpg" alt="Slide 3" />,
-];
+import Hero from "../containers/Hero";
+
+// import Carousel from "./components/carousel";
+
+// const items = [
+//   <img src="image1.jpg" alt="Slide 1" />,
+//   <img src="image2.jpg" alt="Slide 2" />,
+//   <img src="image3.jpg" alt="Slide 3" />,
+// ];
+
 export default function Home() {
   return (
     <>
     <h1>Chabbhikar</h1>
-    <Carousel items={items}/>
+    <Hero />
+    {/* <Carousel items={items}/> */}
     </>
   );
 }
---
+++ apps/web/containers/Hero.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import heroOne from "../assets/heroOne.png";
+
+const Hero = () => {
+  return (
+    <div className="">
+      <Image src={heroOne} alt="" />
+    </div>
+  );
+};
+
+export default Hero;
