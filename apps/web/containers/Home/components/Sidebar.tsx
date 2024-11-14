422aab09cde8a061bf7ac0f1ff61e3d9fac3cb88
mkbhru <mkbhru@gmail.com>
2024-11-14 20:54:49 +0530

    Quote in progress

--- apps/web/containers/Home/components/Sidebar.tsx
+++ apps/web/containers/Home/components/Sidebar.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client"
+import { Drawer, DrawerBody, DrawerOverlay, DrawerContent, useDisclosure, Button, Box, VStack, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Icon } from "@iconify/react";
+// import { SignInForm } from "../../../sign-in/sign-in";
+import styles from "./Sidebar.module.scss";
 
-function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+export default function Sidebar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [toggle, setToggle] = useState({ main: true, login: false });
+
+    const renderMain = (
+        <VStack spacing={6} className={styles.sidebarContainer}>
+            <Heading as="h2" className={styles.title}>MENU</Heading>
+            <Box className={styles.mainContent}>
+                <NavLink className={styles.menuItem} to='/quote'>GET A QUOTE IN 30 MINUTES</NavLink>
+                <Button variant="link" className={styles.menuItem}>CONTACT US NOW</Button>
+            </Box>
+            <Text className={styles.loginLink} onClick={() => setToggle((prev) => ({ ...prev, login: true }))}>
+                MEMBERS LOGIN
+            </Text>
+        </VStack>
+    );
+
+    return (
+        <>
+            {!toggle.main && (
+                <Box className={styles.menuButton} onClick={() => setToggle((prev) => ({ ...prev, main: true }))}>
+                    <Icon icon='material-symbols:menu' width={40} />
+                </Box>
+            )}
+
+            <Drawer isOpen={toggle.main} onClose={onClose} placement="right">
+                <DrawerOverlay />
+                <DrawerContent className={styles.sidebarContainer}>
+                    <DrawerBody>
+                        {toggle.login ? (
+                            <Box display="grid" gridTemplateColumns="1fr 1fr">
+                                <Box bg="white" color="black" w="20rem" p={3} display="flex" flexDirection="column" justifyContent="center">
+                                    <SignInForm />
+                                </Box>
+                                {renderMain}
+                            </Box>
+                        ) : (
+                            renderMain
+                        )}
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </>
+    );
 }
-
-export default Sidebar
