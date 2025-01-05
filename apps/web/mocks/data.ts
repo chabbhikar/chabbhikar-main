cc43df630978cde12f1ae972d75a6fecfedb73cc
mkbhru <mkbhru@gmail.com>
2025-01-05 12:22:44 +0530

    added data.ts in mocks

---
+++ apps/web/mocks/data.ts
@@ -0,0 +1,10 @@
+export const SiteData: { [key: string]: { brandName: string; brandremark: string } } = {
+  ['en-US']: {
+    brandName: '',
+    brandremark: '',
+  },
+  ['cn']: {
+    brandName: '',
+    brandremark: '',
+  },
+};
